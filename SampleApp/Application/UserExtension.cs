8b9d4e78cdfeba5a62bd49e7cdb2cce20cdf7444
robbz <17ip214@git.scc>
2023-12-04 15:17:07 +0300

    регистрация

---
+++ SampleApp/Application/UserExtension.cs
@@ -0,0 +1,13 @@
+﻿using SampleApp.Models;
+
+namespace SampleApp.Application
+{
+    public static class UserExtension
+    {
+        public static bool IsPasswordConfirmation(this User user)
+        {
+            return (user.Password == user.PasswordConfirmation) ? true : false;
+        }
+
+    }
+}
