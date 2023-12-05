cf3cc4b4a1df6392c54602bb870f75d3c969eb57
robbz <17ip214@git.scc>
2023-12-05 11:29:09 +0300

    Начал делать тесты

--- SampleApp.Test/PasswordConfirmationTest.cs
+++ SampleApp.Test/PasswordConfirmationTest.cs
@@ -1,16 +1,57 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+
 namespace SampleApp.Test
 {
     public class Tests
     {
+        private readonly SampleAppContext _db;
+        private IWebDriver driver;
+        private const string LOGIN = "robbz";
+        private const string PASSWORD = "1111";
+        private string EMAIL;
+        private const string PASSWORD_CONFIRMATION = "1111";
+
         [SetUp]
-        public void Setup()
+        public void Init()
         {
+            //driver = new ChromeDriver(Directory.GetCurrentDirectory());
+            driver = new ChromeDriver("C:/group/ip214/webapp/SampleApp");
+            driver.Navigate().GoToUrl("https://localhost:7011/Sign");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
         }
 
         [Test]
-        public void Test1()
+        public void PasswordConfirmationEqualTest()
         {
-            Assert.Pass();
+            Random random = new Random();
+            string s = "",
+            IWebElement userNameElement = driver.FindElement(By.Id("user-name"));
+            IWebElement passwordElement = driver.FindElement(By.Id("password"));
+            IWebElement registerBtn = driver.FindElement(By.Id("submit-btn"));
+
+            userNameElement.SendKeys(LOGIN);
+            passwordElement.SendKeys(PASSWORD);
+            registerBtn.Click();
+            System.Threading.Thread.Sleep(2000);
+            
+
+
+
+
         }
     }
 }
