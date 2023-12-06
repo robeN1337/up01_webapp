b757aee283c0d6e8b5e01b069673416efb0aca55
robbz <17ip214@git.scc>
2023-12-06 16:20:38 +0300

    Добавление авторизации(x2)

---
+++ SampleApp/Pages/Auth.cshtml.cs
@@ -0,0 +1,44 @@
+using Core.Flash;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SampleApp.Models;
+
+namespace SampleApp.Pages
+{
+    public class AuthModel : PageModel
+    {
+        private readonly SampleAppContext _db;
+        private readonly ILogger<SampleAppContext> _logger;
+        private readonly IFlasher _f;
+
+        public AuthModel(SampleAppContext db, ILogger<SampleAppContext> logger, IFlasher f)
+        {
+            _db = db;
+            _logger = logger;
+            _f = f;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public User Input { get; set; }
+
+        public IActionResult OnPost()
+        {
+            User current_user = _db.Users.Where(u => u.Email == Input.Email && u.Password == Input.HashPassword(Input.Password)).FirstOrDefault<User>() as User;
+            if (current_user != null)
+            {
+
+                _f.Flash(Types.Success, $"Добро пожаловать, {current_user.Name}!", dismissable: true);
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                _f.Flash(Types.Danger, $"Неверный логин или пароль!", dismissable: true);
+                return Page();
+            }
+        }
+    }
+}
