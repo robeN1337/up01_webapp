ecfad26ffade915c462f9b8926195549e71300ab
robbz <17ip214@git.scc>
2023-12-07 11:30:06 +0300

    profile, edit fix

--- SampleApp/Pages/Profile.cshtml.cs
+++ SampleApp/Pages/Profile.cshtml.cs
@@ -8,16 +8,13 @@ namespace SampleApp.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
-        {
-        }
 
         private readonly SampleAppContext _context;
         private readonly IFlasher _f;
         private readonly ILogger<EditModel> _log;
 
-        User CurrentUser { get; set; }
-        User ProfileUser { get; set; }
+        public User CurrentUser { get; set; }
+        public User ProfileUser { get; set; }
 
         public ProfileModel(SampleAppContext context, IFlasher f, ILogger<EditModel> log)
         {
@@ -25,5 +22,15 @@ namespace SampleApp.Pages
             _f = f;
             _log = log;
         }
+
+        public async Task<IActionResult> OnGetAsync([FromRoute] int? id)
+
+         {
+
+           ProfileUser = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+           var sessionId = HttpContext.Session.GetString("SampleSession");
+           CurrentUser = await _context.Users.FirstOrDefaultAsync(m => m.Id == Convert.ToInt32(sessionId));
+           return Page();
+         }
     }
 }
