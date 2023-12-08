a5f71573805d16a69e9f2fdf78b63fd9ecb272d9
robbz <17ip214@git.scc>
2023-12-08 13:16:39 +0300

    перенёс сообщения с профиля на индекс(недоделаны)

--- SampleApp/Pages/Index.cshtml.cs
+++ SampleApp/Pages/Index.cshtml.cs
@@ -1,22 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using Core.Flash;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SampleApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SampleApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly SampleAppContext _context;
+        private readonly IFlasher _f;
+        private readonly ILogger<EditModel> _log;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public User CurrentUser { get; set; }
+        public User ProfileUser { get; set; }
+        public bool IsFollow { get; set; }
+
+        public IndexModel(SampleAppContext context, IFlasher f, ILogger<EditModel> log)
         {
-            _logger = logger;
+            _context = context;
+            _f = f;
+            _log = log;
         }
 
-
-
-    public void OnGet()
+        public async Task<IActionResult> OnGetAsync([FromRoute] int? id)
         {
+            var sessionId = HttpContext.Session.GetString("SampleSession");
+            ProfileUser = await _context.Users.Include(u => u.Microposts).FirstOrDefaultAsync(m => m.Id == id) as User;
+            CurrentUser = await _context.Users.Include(u => u.Microposts).FirstOrDefaultAsync(m => m.Id.ToString() == sessionId) as User;
+
+            // если текущий пользователь подписан на профиль пользователя
+            var result = _context.Relations.Where(r => r.Follower == CurrentUser && r.Followed == ProfileUser).FirstOrDefault();
+
+            if (result != null)
+            {
+                IsFollow = true;
+            }
+            else
+            {
+                IsFollow = false;
+            }
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(string message)
+        {
+            var sessionId = HttpContext.Session.GetString("SampleSession");
+            CurrentUser = await _context.Users.Include(u => u.Microposts).FirstOrDefaultAsync(m => m.Id == Convert.ToInt32(sessionId));
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                var m = new Micropost()
+                {
+                    Content = message,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                    UserId = CurrentUser.Id,
+                    //User = this.User
+                };
+
+                try
+                {
+                    _context.Microposts.Add(m);
+                    _context.SaveChanges();
+                    _f.Flash(Types.Success, $"Tweet!", dismissable: true);
+                    return RedirectToPage();
+                }
+                catch (Exception ex)
+                {
+                    _log.Log(LogLevel.Error, $"Ошибка создания сообщения: {ex.InnerException.Message}");
+                }
+
+
+                return Page();
+            }
+            else
+            {
+                return Page();
+            }
 
         }
     }
